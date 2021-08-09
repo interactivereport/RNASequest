2175222e2a3f8a64305869b089dac153d5c2e032
z5ouyang <z5ouyang@ucsd.edu>
2021-08-09 14:17:13 -0400

    fix a bug when gtf name is irregular

--- src/getAnnotation.R
+++ src/getAnnotation.R
@@ -1,12 +1,16 @@
 getAnnotation <- function(strF,strPath=NULL){
     if(grepl("json$",strF)){
         gConfig <- rjson::fromJSON(file=strF)
-        gtfPath <- paste0(strPath,"/rnaseq/",
-                          gConfig$global_params$reference$species,
-                          "/",gConfig$global_params$reference$version,
-                          "/",gConfig$global_params$reference$version,
-                          ".transcript.gtf.gz")
-        strF <- gsub("gtf.gz","gene_info.csv",gtfPath)
+        #gtfPath <- paste0(strPath,"/rnaseq/",
+        #                  gConfig$global_params$reference$species,
+        #                  "/",gConfig$global_params$reference$version,
+        #                  "/",gConfig$global_params$reference$version,
+        #                  ".transcript.gtf.gz")
+        gtfPath <- list.files(paste0(strPath,"/rnaseq/",
+                                     gConfig$global_params$reference$species,
+                                     "/",gConfig$global_params$reference$version),
+                              "gtf.gz$",full.names=T)
+        strF <- gsub("gtf.gz$","gene_info.csv",gtfPath)
         if(!file.exists(strF)){
             gtf.gz_to_gene_info(gtfPath)
         }
